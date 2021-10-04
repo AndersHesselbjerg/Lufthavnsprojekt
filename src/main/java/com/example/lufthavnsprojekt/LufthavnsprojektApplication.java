4e16d2e75d9c52990ed31efff2dd2b891dda3a65
Anders <ande8548@stud.kea.dk>
2021-10-04 15:12:28 +0200

    Initial commit

---
+++ src/main/java/com/example/lufthavnsprojekt/LufthavnsprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.lufthavnsprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LufthavnsprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LufthavnsprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lufthavnsprojekt/LufthavnsprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lufthavnsprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LufthavnsprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
