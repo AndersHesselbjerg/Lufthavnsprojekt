b33c74c9b0f1f6c22429aff9d7a417dd43abc8b0
Ayo <ayokrohn@gmail.com>
2021-10-06 14:55:40 +0200

    Added Constructors, getters/setters and JPA annotation to models.

--- src/main/java/com/example/lufthavnsprojekt/Models/Gate.java
+++ src/main/java/com/example/lufthavnsprojekt/Models/Gate.java
@@ -1,9 +1,47 @@
 package com.example.lufthavnsprojekt.Models;
 
+import javax.persistence.*;
+
+@Entity
 public class Gate {
 
-    private int standplads;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private int gateNo;
+    @Column(nullable = false)
+    private int gateSize;
 
+    public Gate() {
+    }
 
+    public Gate(int gateNo, int gateSize) {
+        this.gateNo = gateNo;
+        this.gateSize = gateSize;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGateNo() {
+        return gateNo;
+    }
+
+    public void setGateNo(int gateNo) {
+        this.gateNo = gateNo;
+    }
+
+    public int getGateSize() {
+        return gateSize;
+    }
+
+    public void setGateSize(int gateSize) {
+        this.gateSize = gateSize;
+    }
 }
